993c8215a07ee15b7b297c00d2ce01fe911c7b0f
alex <2426671397@qq.com>
2019-11-11 12:18:18 +0800

    3
    Signed-off-by: alex <2426671397@qq.com>

---
+++ 真题/2013/省赛/C_C++/C组/3/solve.cpp
@@ -0,0 +1,17 @@
+//
+// Created by alex on 2019/11/11.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int f(int x, int y) {
+    if (x == 3 || y == 4) return 1;
+    return f(x + 1, y) + f(x, y + 1);
+}
+
+int main() {
+    cout << f(0, 0) << endl;
+    return 0;
+}
