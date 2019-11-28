5484da0efcd2e3dc1961b13dce971b3a3cb2e541
wangdongliang1 <57529520+wangdongliang1@users.noreply.github.com>
2019-11-28 13:24:53 +0800

    王东梁-19103117

---
+++ BlueBridgeCup/真题/2013/省赛/C_C++/B组/2/2013B-2.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+       int a,b,c,d,e,x,y,m,n,i=0;
+
+       for(a=1;a<10;a++)
+
+       for(b=1;b<10;b++)
+
+       for(c=1;c<10;c++)
+
+       for(d=1;d<10;d++)
+
+       for(e=1;e<10;e++)
+
+       {
+	   m=a*10+b;n=c*100+d*10+e;x=a*100+d*10+b;y=c*10+e;
+
+       if(m*n==x*y&&a!=b&&a!=c&&a!=d&&a!=e&&b!=c&&b!=d&&b!=e&&c!=d&&c!=e&&d!=e)
+ 
+       i++;
+	   }
+       printf("%d",i)£»
+}
